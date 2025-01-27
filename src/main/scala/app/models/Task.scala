574cf533e9738f569c43038464674dc1c8169300
lipesc <felipedrhouse@hotmail.com>
2025-01-27 18:35:44 -0300

    add task model

--- src/main/scala/app/models/Task.scala
+++ src/main/scala/app/models/Task.scala
@@ -1,3 +1,21 @@
-class Task {
-  
-}
+package models
+
+import slick.jdbc.PostgresProfile.api._
+import java.time.LocalDateTime
+
+
+
+case class Task (
+  id: Option[Long],
+  user_id: Long,
+  title: String,
+  description: Option[String],
+  status: String,
+  priority: String,
+  due_date: Option[LocalDateTime]
+) 
+
+// class TaskTable(tag: Tag) extends Table[Task](tag, "tasks") {
+//   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+//   def
+// }
--- src/main/scala/app/models/User.scala
+++ src/main/scala/app/models/User.scala
@@ -1,5 +1,6 @@
 package models
-
+import slick.jdbc.PostgresProfile.api._
+import java.time.LocalDateTime
 
 case class User(id: Option[Long], username: String, email: String, passwordHash: String)
 
@@ -8,7 +9,7 @@ class UserTable(tag: Tag,) extends Table[User](tag, "users") {
   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
   def username = column[String]("username", O.Unique)
   def email = column[String]("email", O.Unique)
-  def passwordHash = column[String]("password_Hash")
+  def passwordHash = column[String]("password_hash")
 
   def * = (id.?, username, email, passwordHash) <> (User.Tupled, User.unapply)
 }
