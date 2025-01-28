2a6e55d862172126539c6d3cc8ffe6218b28cb19
lipesc <felipedrhouse@hotmail.com>
2025-01-28 19:58:58 -0300

    add userDao. implementado o db postgres para ser usado com o contexto de execução do scala que é assincrono

--- src/main/scala/app/DAO/UserDAO.scala
+++ src/main/scala/app/DAO/UserDAO.scala
@@ -1,3 +1,28 @@
-class UserDAO {
-  
-}
+package DAO
+
+import scala.concurrent.ExecutionContext
+import slick.jdbc.PostgresProfile.api._
+import models.UserTable
+import scala.concurrent.Future
+import models.User
+
+class UserDAO(db: Database)(implicit ec: ExecutionContext) {
+  private  val users = TableQuery[UserTable]
+
+
+
+def createUser(user: User): Future[Long] =
+  db.run((users returning users.map(_.id)) += user)
+
+
+def findUserID(id: Long): Future[Option[User]] = 
+  db.run(users.filter(_.id === id).result.headOption)
+
+
+def findUserName(username: String): Future[Option[User]] =
+  db.run(users.filter(_.username === username).result.headOption)
+
+
+
+
+} 
